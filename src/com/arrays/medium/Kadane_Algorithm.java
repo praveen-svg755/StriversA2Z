22e1d80b4893d0304e4ff4c5bbee035227b23aa6
praveen-svg755 <thiruveedipraveen97@gmail.com>
2025-12-04 21:06:09 +0530

    array medium

--- src/com/arrays/medium/Kadane_Algorithm.java
+++ src/com/arrays/medium/Kadane_Algorithm.java
@@ -48,11 +48,39 @@ public class Kadane_Algorithm {
         return maxSum;
     }
 
+    static void kadanes4(int[] arr){
+        int n = arr.length;
+        int maxSum =Integer.MIN_VALUE;
+        int sum = 0;
+        int tempstart = 0 ,start = -1 , end = -1;
+        for(int i=0 ; i<n ; i++){
+
+            if (sum == 0) tempstart = i;
+
+            sum += arr[i];
+            if(sum>maxSum){
+                maxSum = sum;
+                start = tempstart;
+                end = i;
+            }
+            if(sum <0){
+                sum =0;
+            }
+
+        }
+        System.out.println(start);
+        System.out.println(end);
+        for (int i=start; i<end ; i++){
+          System.out.print(arr[i]+" ");
+       }
+    }
+
     public static void main(String[] args) {
         int[] nums = {-2, -3, 4, -1, -2, 1, 5, -3};
 
         System.out.println(kadanes1(nums));
         System.out.println(kadanes2(nums));
         System.out.println(kadanes3(nums));
+        kadanes4(nums);
     }
 }
